4269eb625f90fd668ee0a0cf126b85c8bdfab37d
szezso <szezsoo@gmail.com>
2013-01-20 19:29:18 +0100

    MODded BoardConfig for proper CWM build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,3 +86,6 @@ BOARD_HAS_NO_MISC_PARTITION := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
 
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# for recovery
+BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun0/file
